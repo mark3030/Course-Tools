d47b4443d8e787612d06ee96efc7a0028b183972
JunzhaoSun <sunjunzhao@innev.cn>
2022-12-15 08:39:12 +0800

    优化文件获取

--- src/pages/Courseware/index.jsx
+++ src/pages/Courseware/index.jsx
@@ -1,6 +1,6 @@
 import { formatFileSize } from '@common/utils/formatFileSize';
 import OSS from 'ali-oss';
-import { Button, List } from 'antd';
+import { Button, Col, List, Radio, Row } from 'antd';
 import React, { useEffect, useState } from "react";
 
 // const ENDPOINT = "oss-cn-shenzhen";
@@ -8,31 +8,58 @@ import React, { useEffect, useState } from "react";
 // const ACCESS_KEY = "LTAIC3GffIXUFtwG";
 // const SECRET_KEY = "v6OSaj6S54bQesM8FGG1nWcbyzNN1z";
 
-const ENDPOINT = "oss-cn-shenzhen";
-const DEFAULT_BUCKET = "dtedu-public";
-const ACCESS_KEY = "vkwoA5pZk7PdSXg1";
-const SECRET_KEY = "yRb7qCBRsqM60DS5Pee8AJIEKmYtch";
+const platfroms = {
+    custom: {
+        region: "oss-cn-shenzhen",
+        bucket: "ediacaran",
+        accessKeyId: "LTAIC3GffIXUFtwG",
+        accessKeySecret: "v6OSaj6S54bQesM8FGG1nWcbyzNN1z",
 
-const CDN_HOST = "https://public.data.dev.dt-pf.com";
+        prefix: "office365/data/",
+        i: 30516
+    },
+    test: {
+        region: "oss-cn-shenzhen",
+        bucket: "dtedu-public",
+        accessKeyId: "vkwoA5pZk7PdSXg1",
+        accessKeySecret: "yRb7qCBRsqM60DS5Pee8AJIEKmYtch",
 
+        prefix: "office365/data/",
+        i: 30516
+    },
+    online: {
+        region: "oss-cn-beijing",
+        bucket: "platform-prod-private",
+        accessKeyId: "LTAI4G5qD9cKKtdPApKiMpHE",
+        accessKeySecret: "TVSaRu9xiqYe4o0BUoLUdP4MPoztYo",
+
+        prefix: "platform-file/office365/data/",
+        i: 30555
+    }
+};
 
 export default () => {
     const [list, setList] = useState([]);
-    const prefix = "office365/data/";
+    const [mode, setMode] = useState({
+        type: "test",
+        prefix: "office365/data/",
+        i: 30516
+    });
 
     useEffect(() => {
-        const client = new OSS({
-            region: ENDPOINT,
-            accessKeyId: ACCESS_KEY,
-            accessKeySecret: SECRET_KEY,
-            bucket: DEFAULT_BUCKET
-          });
+        handleModeChange(mode.type);
+    }, []);
 
-        client.list({ prefix: prefix })
+    const handleModeChange = type => {
+        const { i, prefix, ...options } = platfroms[type];
+
+        new OSS(options)
+            .list({ prefix })
             .then(result => {
                 const list = [];
                 result.objects.forEach(item => {
-                    const { name, size, url } = item;
+                    let { name, size, url } = item;
+                    url = url.replace("platform-prod-private.oss-cn-beijing.aliyuncs.com", "cdn.dt-pf.com");
                     const pact = url.split('://');
                     const ssl = pact[0] === "http" ? 0 : 1;
                     const extIndex = name.lastIndexOf('.');
@@ -46,22 +73,34 @@ export default () => {
                 return list;
             })
             .then(setList);
-    }, []);
+            
+        setMode({ type, prefix, i });
+    };
 
     return (
         <List
             dataSource={list}
             style={{ margin: 50 }}
-            header={<div>课件列表</div>}
+            header={
+                <Row>
+                    <Col span={8}>课件列表</Col>
+                    <Col span={8} offset={8} style={{textAlign: 'right'}}>
+                        <Radio.Group onChange={e => handleModeChange(e.target.value)} value={mode.type} style={{ marginBottom: 8 }}>
+                            <Radio.Button value="test">测试环境</Radio.Button>
+                            <Radio.Button value="online">正式环境</Radio.Button>
+                        </Radio.Group>
+                    </Col>
+                </Row>
+            }
             pagination={true}
             renderItem={item => 
                 <List.Item key={item.etag}>
                     <List.Item.Meta
-                        title={item.name.replace(prefix, "")}
+                        title={item.name.replace(mode.prefix, "")}
                         description={formatFileSize(item.size)}
                     />
-                    <Button href={`https://itest.ow365.cn/?i=30516&n=${item.n}&ssl=${item.ssl}&furl=${item.url}`} target="_blank">OW365测试环境</Button>
-                    <Button href={`https://ow365.cn/?i=30516&n=${item.n}&ssl=${item.ssl}&furl=${item.url}`} target="_blank">OW365正式环境</Button>
+                    <Button href={`https://itest.ow365.cn/?i=${mode.i}&n=${item.n}&ssl=${item.ssl}&furl=${item.url}`} target="_blank">OW365测试环境</Button>
+                    <Button href={`https://ow365.cn/?i=${mode.i}&n=${item.n}&ssl=${item.ssl}&furl=${item.url}`} target="_blank">OW365正式环境</Button>
                     <Button href={`http://office.necibook.com:8884/?pct=1&officeType=zjyz&n=${item.n}&ssl=${item.ssl}&furl=${item.url}`} target="_blank">中教云打开</Button>
                     <Button href={`https://view.officeapps.live.com/op/view.aspx?src=${item.url}`} target="_blank">微软打开</Button>
                 </List.Item>
